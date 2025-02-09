0595b4538a2bdcdd26f3488dd6ef4d8bad50a84a
gauthierseyzeriat <gseyzeriat1@gmail.com>
2025-02-09 11:18:38 +0100

    'Add compose and bruno files'

---
+++ LangUp/Migrations/20250209095247_Init.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LangUp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Translations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    OriginalText = table.Column<string>(type: "text", nullable: false),
+                    TranslatedText = table.Column<string>(type: "text", nullable: false),
+                    SourceLanguage = table.Column<string>(type: "text", nullable: false),
+                    TargetLanguage = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Translations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Translations");
+        }
+    }
+}
